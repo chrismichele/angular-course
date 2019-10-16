76685ebace3e29561a19ef0096106c264048f52f
Christie <mysteriousandspooky@gmail.com>
2019-10-15 23:49:17 -0400

    Final submission

--- mod1/app.js
+++ mod1/app.js
@@ -3,11 +3,35 @@
 
 angular.module('myFirstApp', [])
 
-.controller('MyFirstController', function ($scope) {
-  $scope.name = "Yaakov";
-  $scope.sayHello = function () {
-    return "Hello Coursera!";
+.controller('LunchCheckController', function ($scope) {
+  $scope.textBox = "What do you eat for lunch?";
+  $scope.message = "Click button to test.";
+  $scope.listLunch = "";
+  $scope.message = "";
+
+  $scope.displayMessage = function () {
+    var mymessage = checkList($scope.listLunch);
+    $scope.message = mymessage;
   };
+
+
+  function checkList(string) {
+    var myresult = "";
+    var mylist = string.split(',');
+    var listcount = 0;
+
+    // debug
+    console.log('List: '+mylist);
+    console.log(listcount+' items');
+
+    // return result
+    if(listcount>0 && listcount<=3) myresult='Enjoy!';
+    else if(listcount>3) myresult='Too much!';
+    else if(listcount==0) myresult='Please enter data first';
+
+    return myresult;
+  }
+
 });
 
 })();
